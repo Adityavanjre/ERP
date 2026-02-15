4d002cf21892b039b35ef9739cca87dcdbbc6995
Adityavanjre <adityavanjre280@gmail.com>
2026-02-15 12:09:10 +0530

    fix(nexus): Enable trailingSlash in Frontend and add Health Check to Backend

--- nexus/backend/src/app.service.ts
+++ nexus/backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Nexus ERP Engine v2.0 is Online 🟢';
   }
 }
