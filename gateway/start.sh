f5ddd4891e129596e60da7049fe99509ff59344a
Adityavanjre <adityavanjre280@gmail.com>
2026-02-15 11:55:05 +0530

    fix(deploy): Switch Gateway to use Public HTTPS URLs (.onrender.com) to bypass internal network failures

--- gateway/start.sh
+++ gateway/start.sh
@@ -7,7 +7,7 @@ printenv | grep -E "_HOST|_PORT"
 echo "🚀 Generating Nginx config from template..."
 
 # Use envsubst to replace ONLY the service variables
-export VARS='$AGENCY_VIEWER_HOST $AGENCY_VIEWER_PORT $NEXUS_FRONTEND_HOST $NEXUS_FRONTEND_PORT $NEXUS_BACKEND_HOST $NEXUS_BACKEND_PORT $AGENCY_BACKEND_HOST $AGENCY_BACKEND_PORT'
+export VARS='$AGENCY_VIEWER_HOST $NEXUS_FRONTEND_HOST $NEXUS_BACKEND_HOST $AGENCY_BACKEND_HOST'
 
 envsubst "$VARS" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
