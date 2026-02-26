f8a7570336895b2e985cfff167ccd853a4f3d926
Adityavanjre <adityavanjre280@gmail.com>
2026-02-26 11:21:36 +0530

    fix: disable RN new architecture and resolve TraceService DI in PurchasesModule

--- nexus/backend/src/purchases/purchases.module.ts
+++ nexus/backend/src/purchases/purchases.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { PurchasesService } from './purchases.service';
 import { PurchasesController } from './purchases.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-
 import { AccountingModule } from '../accounting/accounting.module';
+import { SystemModule } from '../system/system.module';
 
 @Module({
-  imports: [PrismaModule, AccountingModule],
+  imports: [PrismaModule, AccountingModule, SystemModule],
   controllers: [PurchasesController],
   providers: [PurchasesService],
 })
-export class PurchasesModule {}
+export class PurchasesModule { }
